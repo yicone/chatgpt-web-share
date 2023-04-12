d99677f62024243965e8cae790911b1e98fbe3da
yicone <yicone@gmail.com>
2023-04-12 17:17:36 +0800

    feat: beautify the home page

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -5,17 +5,79 @@
     </div>
     <div v-else class="justify-center flex-1">
       <div>
-        <n-h1 style="padding-top: 60px;font-family: Metropolis,sans-serif;font-size: 64px !important;">智享无界</n-h1>
-        <p>网络不通畅？注册被拒？绑卡被拒？频繁的证明你是人类？😫</p>
-        <p>Share GPT 为你提供快速，稳定，隐私安全，高性价比的 ChatGPT AI问答服务 🎉</p>
+        <n-space vertical size="large">
+          <n-card class="custom-background">
+            <!-- 在此处放置您的内容 -->
+            <n-h1 style="padding-top: 60px;font-family: Metropolis,sans-serif;font-size: 64px !important;">{{
+              $t("commons.siteSlogan") }}</n-h1>
+          </n-card>
+          <!-- 痛点部分 -->
+          <n-card hoverable>
+            <div class="card-content">
+              <div class="section-title">想要访问 ChatGPT?</div>
+              <ul class="custom-list">
+                <li>无法或难以访问ChatGPT服务，网络速度非常慢。</li>
+                <li>无法收到短信验证码，导致无法完成注册。</li>
+                <li>订阅 Plus 需要绑定信用卡，但受限制国家的信用卡可能被拒绝。</li>
+                <li>频繁遇到人机验证，影响使用体验。</li>
+              </ul>
+            </div>
+          </n-card>
+          <n-card :bordered="false" class="items-center mt-12">
+            <h2>
+              ShareGPT 为你提供
+            </h2>
+          </n-card>
+          <n-grid cols="1 400:2 600:4" x-gap="12" :y-gap="8" item-responsive>
+            <n-grid-item>
+              <n-card hoverable title="开箱即用">
+                <template #header-extra>
+                </template>
+                <div class="card-content">
+                  <p>注册后立即开始使用。<br>免费账户每日有 10 次问答额度。</p>
+                </div>
+              </n-card>
+            </n-grid-item>
+            <n-grid-item>
+              <n-card hoverable embedded title="流畅，稳定可靠">
+                <template #header-extra>
+                </template>
+                <div class="card-content">
+                  <p>采用优质链路连接。<br>Plus 模型更流畅稳定。</p>
+                </div>
+              </n-card>
+            </n-grid-item>
+            <n-grid-item>
+              <n-card hoverable title="隐私安全">
+                <template #header-extra>
+                </template>
+                <div class="card-content">
+                  <p>与市场上的其他账号共享服务相比，你的聊天记录不会与他人分享。</p>
+                </div>
+              </n-card>
+            </n-grid-item>
+            <n-grid-item>
+              <n-card hoverable embedded title="高性价比">
+                <template #header-extra>
+                </template>
+                <div class="card-content">
+                  <p>只需支付一小部分费用，即可使用 Plus 账号的付费模型，获得更智能、更快速的响应，并避免频繁的人机验证干扰。</p>
+                </div>
+              </n-card>
+            </n-grid-item>
+          </n-grid>
+          <div class="mt-9 mb-9" style="text-align: center;">
+            <n-button type="primary" size="large" class="center mt-6 mb-6" @click="handleStartClick">开始使用</n-button>
+            <p class="text-sm c-zinc-400">遇到问题，请联系客服💁 <n-a href="https://t.me/share_gpt"
+                target="blank">ShareGPT官方合租群</n-a>
+            </p>
+          </div>
+        </n-space>
       </div>
-      <n-button type="primary" class="mt-6 mb-6" @click="handleStartClick">开始使用</n-button>
-      <p class="text-sm c-zinc-400">遇到问题，请联系客服💁 <n-a href="https://t.me/share_gpt" target="blank">ShareGPT官方合租群</n-a></p>
     </div>
     <div class="">
-        
-        <p>© 2023 ShareGPT</p>
-      </div>
+      <p>© 2023 ShareGPT</p>
+    </div>
   </div>
 </template>
 
